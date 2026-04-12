55972876845467873e0034bac0c8aebc410a7862
Ansh Gupta <ansh.gupta@unthinkable.co>
2026-04-12 18:08:14 +0530

    feat: integrating authentication gatekeeper across all the frontend endpoints and their deversion logic

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,27 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Room from "./pages/Room";
 import Onboarding from "./pages/Onboarding";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "./libs/axiosInstance";
 
 const App = () => {
+  const {
+    data: authUser,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["get-authenticated-user"],
+    queryFn: () => axiosInstance.get("/auth/me").then((res) => res.data),
+    retry: false,
+  });
+
+  const isAuthenticatedUser = authUser?.user;
+
   return (
     <div
       className="h-screen flex flex-wrap gap-4 p-8"
@@ -16,27 +30,31 @@ const App = () => {
       <Routes>
         <Route
           path="/"
-          element={<Home />}
+          element={isAuthenticatedUser ? <Home /> : <Navigate to="/login" />}
         />
         <Route
           path="/signup"
-          element={<Signup />}
+          element={isAuthenticatedUser ? <Navigate to="/" /> : <Signup />}
         />
         <Route
           path="/login"
-          element={<Login />}
+          element={isAuthenticatedUser ? <Navigate to="/" /> : <Login />}
         />
         <Route
           path="/dashboard"
-          element={<Dashboard />}
+          element={
+            isAuthenticatedUser ? <Dashboard /> : <Navigate to="/login" />
+          }
         />
         <Route
           path="/onboarding"
-          element={<Onboarding />}
+          element={
+            isAuthenticatedUser ? <Onboarding /> : <Navigate to="/login" />
+          }
         />
         <Route
           path="/room/:id"
-          element={<Room />}
+          element={isAuthenticatedUser ? <Room /> : <Navigate to="/login" />}
         />
       </Routes>
       <Toaster />
--- Frontend/src/pages/Login/index.tsx
+++ Frontend/src/pages/Login/index.tsx
@@ -1,19 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import axiosInstance from "../../libs/axiosInstance";
-
 const Login = () => {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["get-authenticated-user"],
-    queryFn: () => axiosInstance.get("/auth/me").then((res) => res.data),
-    retry: false,
-  });
-
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error</div>;
   return (
     <div>
       <h1>Login</h1>
-      <p>{data.email}</p>
     </div>
   );
 };
